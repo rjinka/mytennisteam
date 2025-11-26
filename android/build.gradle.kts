7b40dead483d1c4d8847b5854b23107c2ce51599
Rama Krishna Jinka <jinkaramakrishna@hotmail.com>
2025-11-25 22:33:02 -0500

    Version incrment

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,29 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
 
-import java.util.Properties
+import com.rjinka.buildsrc.tasks.IncrementVersionTask
 
-// A simple Gradle task to increment the version stored in android/version.properties
-// It increments the last numeric part of VERSION_NAME, and increases VERSION_CODE by 1.
-tasks.register("incrementVersion") {
-    doLast {
-        val versionFile = file("version.properties")
-        val props = Properties()
-        if (versionFile.exists()) {
-            versionFile.inputStream().use { props.load(it) }
-        }
-        val vName = props.getProperty("VERSION_NAME", "1.0")
-        val vCode = props.getProperty("VERSION_CODE", "1").toInt()
-        val parts = vName.split(".").toMutableList()
-        if (parts.isEmpty()) parts.add("0")
-        val lastIndex = parts.size - 1
-        val lastNum = parts[lastIndex].toIntOrNull() ?: 0
-        parts[lastIndex] = (lastNum + 1).toString()
-        val newVName = parts.joinToString(".")
-        val newVCode = vCode + 1
-        props.setProperty("VERSION_NAME", newVName)
-        props.setProperty("VERSION_CODE", newVCode.toString())
-        versionFile.outputStream().use { props.store(it, null) }
-        println("Bumped android version to $newVName (code $newVCode)")
-    }
+// Register a typed, cacheable gradle task that increments the properties file
+tasks.register<IncrementVersionTask>("incrementVersion") {
+    versionFile.set(rootProject.layout.projectDirectory.file("version.properties"))
 }
